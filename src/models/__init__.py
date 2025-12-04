39cd9e91e9320e00740f350b8359f9fcb9cebd1d
Sepehr Masihpour <sepehr.masihpour1381@gmail.com>
2025-12-04 04:15:45 +0330

    bug fix

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,17 @@
 from src.db.base import Base
 from src.models.chat import Chat
+from src.models.market_feed import MarketFeed
+from src.models.order import Order, OrderItem
+from src.models.products import Product, ProductVersion
+
 
 # Alembic needs Base.metadata to see models
-__all__ = ["Base", "Chat"]
+__all__ = [
+    "Base",
+    "Chat",
+    "MarketFeed",
+    "Order",
+    "OrderItem",
+    "Product",
+    "ProductVersion",
+]
