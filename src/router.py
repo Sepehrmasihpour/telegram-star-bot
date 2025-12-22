f7f5064253f64ca17727ce81adbe364ac6662ba7
Sepehr Masihpour <sepehr.masihpour1381@gmail.com>
2025-12-22 04:37:41 +0330

    bug fix

--- src/router.py
+++ src/router.py
@@ -179,7 +179,6 @@ async def telegram_webhook(request: Request, db: Session = Depends(get_db)):
             except Exception as e:
                 logger.error("seraializing_callback_query failed: %s", e)
                 return {"ok": False, "error": "serializing callback failed"}
-            logger.debug(response_params)
             resp = await dispatch_response(request, db, response_params)
             return resp
 
