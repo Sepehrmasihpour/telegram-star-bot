2776063e04b1ae4a583c9834c7a6bee4aa97dcf5
Sepehr Masihpour <sepehr.masihpour1381@gmail.com>
2026-02-20 23:28:59 +0330

    update

--- src/bot/chat_flow.py
+++ src/bot/chat_flow.py
@@ -2,43 +2,39 @@ import re
 from typing import Dict, Any, Optional, Union
 from urllib.parse import urlencode
 from decimal import Decimal
-from src.services.pricing import get_version_price
+
 from sqlalchemy.orm import Session, joinedload
 from sqlalchemy import select
-from src.config import logger
 from src.models import Chat, Product
+
 from src.bot.chat_output import telegram_process_bot_outputs as bot_output
 from src.bot import TgChat
+from src.bot.chat_output import TelegrambotOutputs
+
 from src.crud.products import get_product_version_by_id, get_product_by_id
+from src.crud import order
+from src.crud import user
+
+from src.config import logger
 from src.config import settings
-from src.crud.order import (
-    create_order_with_items,
-    CreateOrderItemIn,
-    delete_order,
-    get_order,
-)
-from src.crud.user import (
-    get_chat_by_chat_id,
-    create_chat,
-    update_chat_by_chat_id,
-    create_user,
-    update_user,
-    update_chat,
-    get_user_by_phone,
-)
+
+from src.services.pricing import get_version_price
 
 _PHONE_PATTERN = re.compile(r"^09\d{9}$")
 
 
 def chat_first_level_authentication(
-    db: Session, data: Optional[TgChat] = None, chat_db: Optional[Chat] = None
+    db: Session,
+    outputs: TelegrambotOutputs,
+    data: Optional[TgChat] = None,
+    chat_db: Optional[Chat] = None,
 ) -> Dict[str, Any] | bool:
     try:
 
-        chat = chat_db or get_chat_by_chat_id(db, chat_id=data.id)
+        chat = chat_db or user.get_chat_by_chat_id(db, chat_id=data.id)
         if chat is None:
-            new_user = create_user(db)
-            create_chat(
+            new_user = user.create_user(db)
+            user.create_chat(
                 db,
                 user_id=new_user.id,
                 chat_id=data.id,
@@ -46,9 +42,9 @@ def chat_first_level_authentication(
                 username=data.username,
             )
 
-            return bot_output.terms_and_conditions(data.id, append=True)
+            return outputs.terms_and_conditions(data.id, append=True)
         if not chat.accepted_terms:
-            return bot_output.terms_and_conditions(chat.chat_id, append=True)
+            return outputs.terms_and_conditions(chat.chat_id, append=True)
         return True
     except Exception as e:
         logger.error(f"chat_first_level_authentication failed: {e}")
@@ -60,7 +56,7 @@ def chat_second_lvl_authentication(db: Session, chat: Chat) -> Dict[str, Any] | 
         if chat.chat_verified is not True:
             user = chat.user
             if not user.phone_number:
-                update_chat(db, chat.id, pending_action="waiting_for_phone_number")
+                user.update_chat(db, chat.id, pending_action="waiting_for_phone_number")
                 return bot_output.phone_number_input(chat.chat_id)
             if not user.phone_number_validated:
                 return bot_output.phone_number_verfication_needed(
@@ -94,7 +90,7 @@ def buy_product(db: Session, chat: Chat, product_id: int) -> Dict | None:
 def edit_phone_number(db: Session, chat: Chat):
     try:
 
-        update_chat(
+        user.update_chat(
             db=db,
             chat_id_pk=chat.id,
             chat_verified=False,
@@ -115,17 +111,17 @@ def buy_product_version(
             return auth
         product_version = get_product_version_by_id(db=db, id=product_version_id)
         product_version_price = get_version_price(version=product_version, db=db)
-        order = create_order_with_items(
+        order_data = order.create_order_with_items(
             db=db,
             user_id=chat.user_id,
-            items=[CreateOrderItemIn(product_version_id=int(product_version_id))],
+            items=[order.CreateOrderItemIn(product_version_id=int(product_version_id))],
             commit=True,
         )
         return bot_output.buy_product_version(
             chat_id=chat.chat_id,
             product_version=product_version,
             price=product_version_price,
-            order_id=order.id,
+            order_id=order_data.id,
         )
     except Exception as e:
         logger.error(f"buy_product_version at chat flow failed:{e}")
@@ -138,23 +134,25 @@ def phone_number_input(db: Session, phone_number: str, chat_data: Chat):
         if not valid_phone_number:
             attempts = chat_data.phone_input_attempt
             if attempts >= 2:
-                update_chat(
+                user.update_chat(
                     db, chat_data.id, phone_input_attempt=0, pending_action=None
                 )
                 return bot_output.max_attempt_reached(chat_data.chat_id)
-            update_chat(db, chat_data.id, phone_input_attempt=attempts + 1)
+            user.update_chat(db, chat_data.id, phone_input_attempt=attempts + 1)
             return bot_output.invalid_phone_number(chat_data.chat_id)
-        user_with_the_same_phone = get_user_by_phone(db, phone_number=phone_number)
+        user_with_the_same_phone = user.get_user_by_phone(db, phone_number=phone_number)
         if (
             user_with_the_same_phone
             and chat_data.user_id != user_with_the_same_phone.id
         ):
-            update_chat(db=db, chat_id_pk=chat_data.id, pending_action=None)
+            user.update_chat(db=db, chat_id_pk=chat_data.id, pending_action=None)
             return bot_output.login_to_acount(
                 chat_id=chat_data.chat_id, phone_number=phone_number
             )
-        updated_chat = update_chat(db=db, chat_id_pk=chat_data.id, pending_action=None)
-        update_user(db=db, user_id=chat_data.user_id, phone_number=phone_number)
+        updated_chat = user.update_chat(
+            db=db, chat_id_pk=chat_data.id, pending_action=None
+        )
+        user.update_user(db=db, user_id=chat_data.user_id, phone_number=phone_number)
         return chat_second_lvl_authentication(db=db, chat=updated_chat)
 
     except Exception as e:
@@ -165,13 +163,13 @@ def phone_number_input(db: Session, phone_number: str, chat_data: Chat):
 def login(db: Session, chat: Chat, phone_number: str):
     try:
 
-        user_to_login_to = get_user_by_phone(db=db, phone_number=phone_number)
+        user_to_login_to = user.get_user_by_phone(db=db, phone_number=phone_number)
         if chat.user.id == user_to_login_to.id:
             return bot_output.already_logged_in(
                 chat_id=chat.chat_id, phone_number=phone_number
             )
 
-        updated_chat = update_chat(
+        updated_chat = user.update_chat(
             db=db, chat_id_pk=chat.id, user_id=user_to_login_to.id, chat_verified=False
         )
         return chat_second_lvl_authentication(db=db, chat=updated_chat)
@@ -184,7 +182,7 @@ def send_otp(db: Session, chat: Chat):
     try:
 
         #! this is a placeholder for when we actually send the otp
-        update_chat(db=db, chat_id_pk=chat.id, pending_action="waiting_for_otp")
+        user.update_chat(db=db, chat_id_pk=chat.id, pending_action="waiting_for_otp")
         return bot_output.phone_numebr_verification(chat_id=chat.chat_id)
     except Exception as e:
         logger.error(f"send_otp at chat flow failed:{e}")
@@ -197,20 +195,20 @@ def otp_verify(db: Session, text: str, chat: Chat):
         if not text == "1111":  #!This is very much a place holder for later
             attemps = chat.otp_input_attempt
             if attemps >= 2:
-                update_chat(
+                user.update_chat(
                     db=db, chat_id_pk=chat.id, otp_input_attempt=0, pending_action=None
                 )
                 return bot_output.max_attempt_reached(chat_id=chat.chat_id)
-            update_chat(db=db, chat_id_pk=chat.id, otp_input_attempt=attemps + 1)
+            user.update_chat(db=db, chat_id_pk=chat.id, otp_input_attempt=attemps + 1)
             return bot_output.invalid_otp(chat.chat_id)
-        update_chat(
+        user.update_chat(
             db=db,
             chat_id_pk=chat.id,
             pending_action=None,
             otp_input_attempt=0,
             chat_verified=True,
         )
-        update_user(db=db, user_id=chat.user_id, phone_number_validated=True)
+        user.update_user(db=db, user_id=chat.user_id, phone_number_validated=True)
         return bot_output.phone_number_verified(chat.chat_id)
     except Exception as e:
         logger.error(f"otp_verify at chat flow failed: {e}")
@@ -226,17 +224,19 @@ def is_last_message(
     try:
         if chat_id is None and chat is None:
             raise ValueError("when chat is None chat_id cannot be None")
-        chat = get_chat_by_chat_id(db, chat_id) if chat is None else chat
+        chat = user.get_chat_by_chat_id(db, chat_id) if chat is None else chat
         if chat is None:
             return False
         last_message_id = chat.last_message_id
         if last_message_id is None:
-            update_chat_by_chat_id(
+            user.update_chat_by_chat_id(
                 db=db, chat_id=chat.chat_id, last_message_id=int(message_id)
             )
             return True
         if int(message_id) > last_message_id:
-            update_chat_by_chat_id(db, chat.chat_id, last_message_id=int(message_id))
+            user.update_chat_by_chat_id(
+                db, chat.chat_id, last_message_id=int(message_id)
+            )
             return True
         if int(message_id) == last_message_id:
             return True
@@ -276,7 +276,7 @@ def get_prices(
 
 def payment_gateway(db: Session, chat: Chat, order_id: Union[str, int]):
     try:
-        order = get_order(db=db, order_id=order_id)
+        order_data = order.get_order(db=db, order_id=order_id)
         order_item = order.items[0]
         product_version_id = order_item.product_version_id
         unit_price = order_item.unit_price
@@ -284,7 +284,7 @@ def payment_gateway(db: Session, chat: Chat, order_id: Union[str, int]):
         product_name = product_version.product.name
         return bot_output.payment_gateway(
             chat_id=chat.chat_id,
-            order_id=order.id,
+            order_id=order_data.id,
             product_name=product_name,
             amount=unit_price,
             pay_url=f"{settings.base_url}/pay?{urlencode({"order_id":order_id})}",
@@ -295,13 +295,13 @@ def payment_gateway(db: Session, chat: Chat, order_id: Union[str, int]):
 
 
 def cancel_order(db: Session, chat: Chat, order_id: Union[int, str]):
-    delete_order(db=db, order_id=order_id)
+    order.delete_order(db=db, order_id=order_id)
     return bot_output.return_to_menu(chat_id=chat.chat_id)
 
 
 def confirm_payment(db: Session, chat: Chat, order_id: Union[int, str]):
-    order = get_order(db=db, order_id=order_id)
-    if order.status != "paid":
+    order_data = order.get_order(db=db, order_id=order_id)
+    if order_data.status != "paid":
         return bot_output.payment_confirmed(chat_id=chat.chat_id, order_id=order_id)
     return bot_output.payment_not_confirmed(chat_id=chat.chat_id, order_id=order_id)
 
